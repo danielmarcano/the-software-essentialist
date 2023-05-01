c6730ad9dbba6c5149cad7cac0abcdd1a9fdb676
Daniel Marcano <danielmarcanodev@gmail.com>
2023-05-01 12:58:33 +0200

    refactor: remove comment from isPalindrome tests as the code now self-explains the use-cases and the edge-cases

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,25 +1,5 @@
 import { isPalindrome } from '.'
 
-/*
-  Create a palindrome checker that should be able to detect that a string is a palindrome; that is, it is the
-  same word or phrase in reverse.This means that words like "mom" and "wow" palindromes. It also means that words
-  like "bill" are not palindromes. It should still know that something is a palindrome, even if the casing is off
-  (that means that "Mom" is still a palindrome). Lastly, it should also be able to detect palindromes
-  in phrases like "Was It A Rat I Saw" and "Never Odd or Even" too.
-
-  Use-cases
-
-  - It returns a boolean if the given string is a palindrome (it is the same word or phrase in reverse) regardless of:
-    - casing
-    - spaces between words
-
-  Edge-cases
-
-  - What happens with special characters and numbers? It might be best to remove them all so that we only run our logic against letters
-  - For strings smaller than 3 digits, we simply return false
-*/
-
-
 describe('palindrome tests', () => {
   it('Returns a boolean', () => {
     expect(typeof isPalindrome('mom')).toBe('boolean');
