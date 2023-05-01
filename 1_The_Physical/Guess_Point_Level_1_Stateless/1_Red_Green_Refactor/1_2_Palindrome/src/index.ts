f6d80a46ffcaf97b7b5adbbf0120373c67694e68
Daniel Marcano <danielmarcanodev@gmail.com>
2023-05-01 12:16:41 +0200

    refactor: palindrome returns false if text passed is smaller than 3-digits-long, so that its logic is not executed unnecessarily

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,4 +1,8 @@
 export function palindrome(text: string) {
+  if (text.length < 3) {
+    return false;
+  }
+
   const parsedText = text.toLowerCase().replace(/ /g, '');
   const reversedText = parsedText.split('').reverse().join('');
 
