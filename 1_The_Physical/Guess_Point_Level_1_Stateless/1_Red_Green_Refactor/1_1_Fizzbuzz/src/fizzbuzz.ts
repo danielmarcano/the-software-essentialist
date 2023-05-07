150d7a3379659e4f0631a128a2f3c1fd1c5e3e53
Daniel Marcano <danielmarcanodev@gmail.com>
2023-05-07 15:08:35 +0200

    [green]: fizzBuzz returns 'FizzBuzz' for multiples of both 3 and 5

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,6 +2,10 @@ export function fizzBuzz(numberToConvert: number) {
   let isMultipleOfThree = numberToConvert % 3 === 0;
   let isMultipleOfFive = numberToConvert % 5 === 0;
 
+  if (isMultipleOfThree && isMultipleOfFive) {
+    return "FizzBuzz";
+  }
+
   if (isMultipleOfThree) {
     return "Fizz";
   }
