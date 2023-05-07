97d6c23f6bf858e127b1af71cc8993720d0ab374
Daniel Marcano <danielmarcanodev@gmail.com>
2023-05-07 20:06:17 +0200

    [red]: isPalindrome test checking that it returns a boolean

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,25 @@
+import { isPalindrome } from '.'
 
-describe('palindrome checker', () => {
+/*
+  Create a palindrome checker that should be able to detect that a string is a palindrome; that is, it is the
+  same word or phrase in reverse.This means that words like "mom" and "wow" palindromes. It also means that words
+  like "bill" are not palindromes. It should still know that something is a palindrome, even if the casing is off
+  (that means that "Mom" is still a palindrome). Lastly, it should also be able to detect palindromes
+  in phrases like "Was It A Rat I Saw" and "Never Odd or Even" too.
 
+  Use-cases
+  - It returns a boolean if the given string is a palindrome (it is the same word or phrase in reverse) regardless of:
+    - casing
+    - spaces between words
+
+  Edge-cases
+  - Since strings like "Was It A Rat I Saw" should return true, we should remove any white-spaces
+  - For strings smaller than 3 digits, we simply return false
+*/
+
+
+describe('isPalindrome tests', () => {
+  it('Returns a boolean', () => {
+    expect(typeof isPalindrome()).toBe('boolean');
+  })
 })
