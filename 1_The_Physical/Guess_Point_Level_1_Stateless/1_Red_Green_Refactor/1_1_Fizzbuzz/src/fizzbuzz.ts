807e7558835a49d42cd5d9219ca7d567dfab45a6
Daniel Marcano <danielmarcanodev@gmail.com>
2023-05-07 15:05:41 +0200

    [green]: fizzBuzz returns 'Fizz' for multiples of 3

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,9 @@
 export function fizzBuzz(numberToConvert: number) {
+  let isMultipleOfThree = numberToConvert % 3 === 0;
+
+  if (isMultipleOfThree) {
+    return "Fizz";
+  }
+
   return String(numberToConvert);
 }
