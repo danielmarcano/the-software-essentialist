6358507855f95306fe66ced9fcd4b2d8084d8b60
Daniel Marcano <danielmarcanodev@gmail.com>
2023-05-20 18:39:42 +0200

    [red]: PasswordValidator.isValid test for checking passwords with invalid length

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,5 +1,15 @@
+import { PasswordValidator } from './';
 
-describe('password validator', () => {
-
+describe('PasswordValidator tests', () => {
+  describe('It knows passwords should be between 5 and 15 characters long', () => {
+    it('Knows that "lal0" has an invalid length', () => {
+      expect(PasswordValidator.isValid('lal0')).toEqual({
+        isValid: false,
+        errors: [{
+          type: 'invalid-length'
+        }]
+      })
+    })
+  })
 })
 
---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -0,0 +1,5 @@
+export class PasswordValidator {
+  static isValid(password: string) {
+    return;
+  }
+}
