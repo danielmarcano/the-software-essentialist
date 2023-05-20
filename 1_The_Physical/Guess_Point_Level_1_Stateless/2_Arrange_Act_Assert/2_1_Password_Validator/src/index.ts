11f185516a6578ff36e93553b4167f26a452d054
Daniel Marcano <danielmarcanodev@gmail.com>
2023-05-20 19:25:05 +0200

    [refactor]: create PasswordValidator.hasValidLength to determine is a password has a valid length

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,6 +1,8 @@
 export class PasswordValidator {
   static DIGITS_REG_EX = /\d/;
   static UPPER_CASE_LETTER_REG_EX = /[A-Z]/;
+  static MINIMUM_LENGTH = 5;
+  static MAXIMUM_LENGTH = 15;
 
   static hasDigits(password: string): boolean {
     return PasswordValidator.DIGITS_REG_EX.test(password);
@@ -8,6 +10,10 @@ export class PasswordValidator {
 
   static hasUpperCaseLetter(password: string): boolean {
     return PasswordValidator.UPPER_CASE_LETTER_REG_EX.test(password);
+  }
+
+  static hasValidLength(password: string): boolean {
+    return password.length >= PasswordValidator.MINIMUM_LENGTH && password.length <= PasswordValidator.MAXIMUM_LENGTH;
   }
 
   static isValid(password: string) {
@@ -29,18 +35,18 @@ export class PasswordValidator {
       }
     }
 
-    if (password === 'Lala0') {
+    if (!PasswordValidator.hasValidLength(password)) {
       return {
-        isValid: true,
-        errors: [],
+        isValid: false,
+        errors: [{
+          type: 'invalid-length',
+        }],
       }
     }
 
     return {
-      isValid: false,
-      errors: [{
-        type: 'invalid-length',
-      }],
+      isValid: true,
+      errors: [],
     };
   }
 }
