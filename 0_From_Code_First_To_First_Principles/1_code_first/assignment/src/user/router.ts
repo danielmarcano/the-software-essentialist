f19041b8c745dba7006d684e4ea4a6f756218a77
Daniel Marcano <danielmarcanodev@gmail.com>
2023-09-13 17:17:34 +0200

    [code-first-ddd-forum]: create /user router to handle its requests through the UserController

--- 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/user/router.ts
+++ 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/user/router.ts
@@ -1,7 +1,18 @@
-import { Router } from 'express';
+import { Request, Router } from 'express';
+import UserController from './controllers/UserController';
+import { PrismaUserRepository } from './repositories/PrismaUserRepository';
+import UserService from './services/UserService';
 
 export const router = Router();
 
-router.post('/new', (req, res) => {
-  // TODO: complete implementation
-})
+interface UserRequest extends Request {
+  userController?: UserController;
+}
+
+router.use((req: UserRequest, _, next) => {
+  req.userController = new UserController(new UserService(new PrismaUserRepository()));
+
+  next();
+});
+
+router.post('/new', (req: UserRequest, res) => req.userController?.createUser(req, res))
